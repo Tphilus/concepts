678cae046fba19be526c2852e2592b65c0c48f17
Tphilus <tkanateydadzie@gmail.com>
2026-04-23 13:58:35 +0000

    add joi validation and config setup; implement app configuration and update app service to use config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import Joi, * as joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelloModule } from './hello/hello.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [HelloModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // makes configmodule globally available
+      validationSchema: joi.object({
+        APP_NAME: Joi.string().default('defaultApp'),
+      }),
+    }),
+    HelloModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
